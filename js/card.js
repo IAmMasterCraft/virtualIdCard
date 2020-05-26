f853e654d4ab8fc84955d534dd9153b709c180a6
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2020-05-27 00:39:17 +0100

    Update card.js

--- js/card.js
+++ js/card.js
@@ -1,7 +1,7 @@
 /*IAmMasterCraft*/
 function downloadPng (userName) {
   html2canvas(document.querySelector("#main-cont")).then(canvas => {
-      document.querySelector("#capture")).appendChild(canvas);
+      document.querySelector("#capture").appendChild(canvas);
       return Canvas2Image.saveAsImage(canvas, null, null, "png", userName)
   });
 }

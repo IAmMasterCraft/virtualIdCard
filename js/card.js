d29c5d0e67141af7f4f022390b3a2c51ce91f8de
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2020-08-15 03:35:45 +0100

    Update card.js

--- js/card.js
+++ js/card.js
@@ -21,10 +21,11 @@ function downloadPdf(userName) {
     doc.addImage(img, 'JPEG', 10, 10, (canvas.width/4), (canvas.height/4));
 //     doc.save(userName + ".pdf");
     var string = doc.output('datauristring');
-    var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>"
-    var x = window.open();
-    x.document.open();
-    x.document.write(iframe);
+//     var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>"
+//     var x = window.open();
+//     x.document.open();
+//     x.document.write(iframe);
+    $("#dl_link").attr("href", string).show();
     downloadFileCallBack("pdf");    
   });
   // doc.text("Hello world!", 10, 10);
